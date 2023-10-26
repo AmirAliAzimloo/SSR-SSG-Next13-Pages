0bfa4c5da6c513a6c7b20dfb1b28ca843406cc61
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-26 19:54:27 +0330

    navbar completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import Navbar from "@/components/modules/Navbar/Navbar";
 import "@/styles/globals.css";
 import {AppProps} from "@/types/appTypes"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Navbar/>
+    <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;

975d5ef9c7654daf8270cdea47361d4824a8060e
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-26 20:24:32 +0330

    about completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/templates/Index/About";
 import Slider from "@/components/templates/Index/Slider";
 import React from "react";
 
 function Index() {
   return (<>
   <Slider/>
+  <About/>
   </>);
 }
 

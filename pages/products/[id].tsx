ddf9eab6f5beaadc6fbd862bc06b7fc00c0d52de
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-27 14:25:21 +0330

    project completed

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,21 +2,22 @@ import "@/styles/Product.module.css";
 
 import Comments from "@/components/templates/Product/Comments";
 import ProductsDetails from "@/components/templates/Product/ProductDetails";
-import { MenuTypes } from "@/types/serverTypes";
+import { CommentsTypes, MenuTypes } from "@/types/serverTypes";
 
 interface ProductProps{
-  product:MenuTypes
+  product:MenuTypes,
+  comments:CommentsTypes[]
 }
 
-const Product = ({ product }:ProductProps) => {
+const Product = ({ product , comments}:ProductProps) => {
   return (
     <>
       <ProductsDetails data={product} />
-      <Comments />
+      <Comments data={comments} />
     </>
   );
 };
-
+ 
 export async function getStaticPaths(context:any) {
   const res = await fetch(`http://localhost:4000/menu`);
   const products:MenuTypes[] = await res.json();
@@ -37,10 +38,21 @@ export async function getStaticProps(context:any) {
   const res = await fetch(`http://localhost:4000/menu/${params.id}`);
   const product:MenuTypes = await res.json();
 
+
+  
+  const commentsResponse = await fetch(`http://localhost:4000/comments`);
+  const comments:CommentsTypes[] = await commentsResponse.json();
+
+  const productComments = comments.filter(
+    (comment) => comment.productID === +params.id
+  );
+
   return {
     props: {
       product,
+      comments: productComments,
     },
+    revalidate: 60 * 60 * 12 // 12h
   };
 }
 
